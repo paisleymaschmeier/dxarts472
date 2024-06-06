b7bc558532e750220e27850b3c15ae25c1a112bb
Paisley Maschmeier <85045220+paisleymaschmeier@users.noreply.github.com>
2024-06-05 20:21:42 -0700

    Add files via upload

--- final_project.ino
+++ final_project.ino
@@ -1,3 +1,10 @@
+// Resources:
+  // https://forum.arduino.cc/t/28byj-48-4-phase-stepper-motor/1162926
+  // https://www.youtube.com/watch?v=LHvXPeT2gws
+  // https://www.youtube.com/watch?v=STCctT6JyPg
+  // https://www.youtube.com/watch?v=-JYi08WKe6c&t=108s
+  
+
 //Includes the Arduino Stepper Library
 #include <Stepper.h>
 
@@ -10,31 +17,42 @@ Stepper myStepper = Stepper(stepsPerRevolution, 8, 10, 9, 11);
 
 #include <Servo.h>
 Servo myservo;
-const int threshValue = 300;
+const int threshValue = 400;
+
+int sensorValue=0;
+int value_average = 0;
+
+unsigned long previousStepperTime = millis();
+long stepperInterval = 0;
 
 void setup()
 {
   Serial.begin (9600);
   myservo.attach(3);
+  myStepper.setSpeed(3);
 }
 
-void loop()
-{
+void loop() {
   int value = analogRead(A3);
+
   Serial.println(value);
 
   if (value < threshValue)
   {
-    myservo.writeMicroseconds(800);
+    int positionLower = map(value, 200, threshValue, 1200, 1599);
+    myservo.writeMicroseconds(positionLower);
+    //myStepper.setSpeed(1);
+	  //myStepper.step(stepsPerRevolution);
   }
   else
   {
-    myservo.writeMicroseconds(2250);
+    int positionHigher = map(value, threshValue+1, 1000, 1600, 1700);
+    myservo.writeMicroseconds(positionHigher);
+    //myStepper.setSpeed(1);
+	  //myStepper.step(stepsPerRevolution);
   }
 
-	// Rotate CW slowly at 5 RPM
-	myStepper.setSpeed(5);
-	myStepper.step(stepsPerRevolution);
+	myStepper.step(-6);
 }
 
 
